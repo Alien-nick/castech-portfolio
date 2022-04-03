9afde39b79007499aed66a3b2e52fed10455a472
Nicholas Seetaram <nicholas.daniel.seetaram@gmail.com>
2022-04-02 23:52:47 -0400

    Add projects

--- app/View/Components/home/projects.php
+++ app/View/Components/home/projects.php
@@ -3,9 +3,13 @@
 namespace App\View\Components\home;
 
 use Illuminate\View\Component;
+use Illuminate\Support\Arr;
 
 class projects extends Component
 {
+
+    public array $items = [];
+    public array $tabs = [];
     /**
      * Create a new component instance.
      *
@@ -13,7 +17,40 @@ class projects extends Component
      */
     public function __construct()
     {
-        //
+        $this->items = [
+            [
+                'category' => ['Laravel', 'Flutter', 'Oracle Apex', 'iOS', 'Android', 'PHP'],
+                'title' => 'Full Stack app with Laravel, Flutter and Oracle Apex',
+                'image' => url('/images/litta-reporta.png'),
+                'url' => 'https://youtu.be/pHIh4EU0MtE'
+            ],
+            [
+                'category' => ['Oracle Apex', 'Laravel', 'PHP'],
+                'title' => 'A student record management system for the Government Technical Institute',
+                'image' => url('/images/gti.png'),
+                'url' => 'https://youtu.be/pHIh4EU0MtE'
+            ],
+            [
+                'category' => ['Wordpress', 'PHP'],
+                'title' => 'A redesign of the Ministry of Tourism Website',
+                'image' => url('/images/mot.png'),
+                'url' => 'https://youtu.be/pHIh4EU0MtE'
+            ],
+            [
+                'category' => ['Laravel', 'PHP'],
+                'title' => 'A Laravel Microservice that was build to showcase covidcase statuses around the world.',
+                'image' => url('/images/bot.jpeg'),
+                'url' => 'https://youtu.be/38fJX4oj9tk'
+            ],
+            [
+                'category' => ['Flutter', 'Oracle Apex', 'iOS', 'Android'],
+                'title' => 'Mobile App for SacPackaging Customers to view packages, invoices, quotes easier.',
+                'image' => url('/images/sac.png'),
+                'url' => '_self'
+            ]
+        ];
+
+        $this->tabs = array_unique(Arr::flatten(Arr::pluck($this->items, 'category')));
     }
 
     /**
