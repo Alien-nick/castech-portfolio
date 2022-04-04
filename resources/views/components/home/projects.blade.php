f191e669b6fc14fed0e42c2cd611e2f61c6ffa5f
Nicholas Seetaram <nicholas.daniel.seetaram@gmail.com>
2022-04-03 20:43:42 -0400

    Add static projects

--- resources/views/components/home/projects.blade.php
+++ resources/views/components/home/projects.blade.php
@@ -1,58 +1,416 @@
-<!-- ====== Project Section Start -->
+<!-- ====== Portfolio Section Start -->
 <section
-  id="projects"
-  x-data="
-        {
-          selectedTab: 'all',
-          activeClasses: 'bg-primary text-white',
-          inactiveClasses: 'text-body-color hover:bg-primary hover:text-white',
-        }
-      "
-  class="pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] bg-white dark:bg-gray-900"
+id="projects"
+x-data="
+  {
+    showCards: 'all',
+    activeClasses: 'bg-primary text-white',
+    inactiveClasses: 'text-body-color hover:bg-primary hover:text-white',
+  }
+"
+class="pt-20 lg:pt-[120px] pb-12 lg:pb-[90px]"
 >
-  <div class="container">
-    <div class="flex flex-wrap -mx-4">
-      <div class="w-full px-4">
-        <div class="text-center mx-auto mb-[60px] max-w-[510px]">
-          <h2 class="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark dark:text-gray-300 mb-4">
-            Software Projects Worked on
-          </h2>
+<div class="container">
+  <div class="flex flex-wrap -mx-4">
+    <div class="w-full px-4">
+      <div class="text-center mx-auto mb-[60px] max-w-[510px]">
+        <span class="font-semibold text-lg text-primary mb-2 block">
+          Our Portfolio
+        </span>
+        <h2
+          class="
+            font-bold
+            text-3xl
+            sm:text-4xl
+            md:text-[40px]
+            text-dark
+            mb-4
+            dark:text-white
+          "
+        >
+          Software Projects
+        </h2>
+      </div>
+    </div>
+  </div>
+  <div class="flex flex-wrap justify-center -mx-4">
+    <div class="w-full px-4">
+      <ul class="flex flex-wrap justify-center mb-12 space-x-1">
+        <li class="mb-1">
+          <button
+            @click="showCards = 'all' "
+            :class="showCards == 'all' ? activeClasses : inactiveClasses "
+            class="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            "
+          >
+            All Projects
+          </button>
+        </li>
+        <li class="mb-1">
+          <button
+            @click="showCards = 'branding' "
+            :class="showCards == 'branding' ? activeClasses : inactiveClasses "
+            class="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            "
+          >
+            Laravel
+          </button>
+        </li>
+        <li class="mb-1">
+          <button
+            @click="showCards = 'design' "
+            :class="showCards == 'design' ? activeClasses : inactiveClasses "
+            class="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            "
+          >
+            Flutter
+          </button>
+        </li>
+        <li class="mb-1">
+          <button
+            @click="showCards = 'marketing' "
+            :class="showCards == 'marketing' ? activeClasses : inactiveClasses "
+            class="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            "
+          >
+            Oracle Apex
+          </button>
+        </li>
+        <li class="mb-1">
+          <button
+            @click="showCards = 'development' "
+            :class="showCards == 'development' ? activeClasses : inactiveClasses "
+            class="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            "
+          >
+            iOS
+          </button>
+        </li>
+        <li class="mb-1">
+          <button
+            @click="showCards = 'development' "
+            :class="showCards == 'development' ? activeClasses : inactiveClasses "
+            class="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            "
+          >
+            Android
+          </button>
+        </li>
+        <li class="mb-1">
+          <button
+            @click="showCards = 'development' "
+            :class="showCards == 'development' ? activeClasses : inactiveClasses "
+            class="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            "
+          >
+            PHP
+          </button>
+        </li>
+        <li class="mb-1">
+          <button
+            @click="showCards = 'development' "
+            :class="showCards == 'development' ? activeClasses : inactiveClasses "
+            class="
+              inline-block
+              py-2
+              md:py-3
+              px-5
+              lg:px-8
+              rounded-lg
+              text-base
+              font-semibold
+              text-center
+              transition
+            "
+          >
+            Wordpress
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="flex flex-wrap -mx-4">
+    <div
+      :class="showCards == 'all' || showCards == 'Flutter' || 'Laravel' || 'Oracle Apex' || 'iOS' || 'Android' || 'PHP' ? 'block' : 'hidden' "
+      class="w-full md:w-1/2 xl:w-1/3 px-4"
+    >
+      <div class="relative mb-12">
+        <div class="rounded-lg overflow-hidden ease-in duration-100 hover:scale-105">
+          <img
+            src="{{ url('/images/litta-reporta.png') }}"
+            alt="LittaReporta"
+            class="w-full h-[260px] object-cover"
+          />
+        </div>
+        <div
+          class="
+          text-center bg-white dark:bg-slate-800 relative z-10 py-9 px-3 rounded-lg shadow-lg mx-7 -mt-4 h-[260px]
+          "
+        >
+          <span class="text-sm text-primary font-semibold block mb-2">
+            Flutter, Laravel, Oracle Apex, iOS, Android, PHP
+          </span>
+          <h3 class="font-bold text-xl text-dark mb-4 dark:text-white">
+            LittaReporta, A Full stack app built using Oracle Apex, Laravel and Flutter.
+          </h3>
+          <a
+            href="https://youtu.be/pHIh4EU0MtE"
+            class="
+              text-body-color text-sm
+              font-semibold
+              py-3
+              px-7
+              inline-block
+              border
+              rounded-md
+              hover:bg-primary hover:border-primary hover:text-white
+              transition
+            "
+          >
+            View Details
+          </a>
         </div>
       </div>
     </div>
-    <div class="flex flex-wrap justify-center -mx-4">
-      <div class="w-full px-4">
-        <ul class="flex flex-wrap justify-center mb-12 space-x-1">
-          <li class="mb-1">
-            <button
-              @click="selectedTab = 'all' "
-              :class="selectedTab == 'all' ? activeClasses : inactiveClasses "
-              class="inline-block py-2 md:py-3 px-5 lg:px-8 rounded-lg text-base font-semibold text-center transition "
-            >
-              All Projects
-            </button>
-          </li>
-          @foreach($tabs as $tab)
-          <li class="mb-1">
-            <button
-              @click="selectedTab = '{{$tab}}' "
-              :class="selectedTab === '{{$tab}}' ? activeClasses : inactiveClasses "
-              class="inline-block py-2 md:py-3 px-5 lg:px-8 rounded-lg text-base font-semibold text-center transition ">
-              {{$tab}}
-            </button>
-          </li>
-          @endforeach
-        </ul>
+    <div
+      :class="showCards == 'all' || showCards == 'Oracle Apex' ? 'block' : 'hidden' "
+      class="w-full md:w-1/2 xl:w-1/3 px-4"
+    >
+      <div class="relative mb-12">
+        <div class="rounded-lg overflow-hidden ease-in duration-100 hover:scale-105">
+          <img
+            src="{{ url('/images/gti.png') }}"
+            alt="GTI Management System"
+            class="w-full h-[260px] object-cover"
+          />
+        </div>
+        <div
+          class="
+          text-center bg-white dark:bg-slate-800 relative z-10 py-9 px-3 rounded-lg shadow-lg mx-7 -mt-4 h-[260px]
+          "
+        >
+          <span class="text-sm text-primary font-semibold block mb-2">
+            Oracle Apex
+          </span>
+          <h3 class="font-bold text-xl text-dark mb-4 dark:text-white">
+            A student record management system for the Government Technical Institute
+          </h3>
+          <a
+            href="https://youtu.be/pHIh4EU0MtE"
+            class="
+              text-body-color text-sm
+              font-semibold
+              py-3
+              px-7
+              inline-block
+              border
+              rounded-md
+              hover:bg-primary hover:border-primary hover:text-white
+              transition
+            "
+          >
+            View Details
+          </a>
+        </div>
       </div>
     </div>
-    <div class="flex flex-wrap -mx-4">
-      @foreach ($items as $item)
-        <x-project-items :title="$item['title']"
-                          :categories="$item['category']"
-                          :image="$item['image']"
-                          :url="$item['url']"></x-project-items>
-      @endforeach
+    <div
+      :class="showCards == 'all' || showCards == 'Wordpress' || 'PHP' ? 'block' : 'hidden' "
+      class="w-full md:w-1/2 xl:w-1/3 px-4"
+    >
+      <div class="relative mb-12">
+        <div class="rounded-lg overflow-hidden ease-in duration-100 hover:scale-105">
+          <img
+            src="{{ url('/images/mot.png') }}"
+            alt="Ministry of Tourism"
+            class="w-full h-[260px] object-cover"
+          />
+        </div>
+        <div
+          class="
+          text-center bg-white dark:bg-slate-800 relative z-10 py-9 px-3 rounded-lg shadow-lg mx-7 -mt-4 h-[260px]
+          "
+        >
+          <span class="text-sm text-primary font-semibold block mb-2">
+            Wordpress, PHP
+          </span>
+          <h3 class="font-bold text-xl text-dark mb-4 dark:text-white">
+            A redesign of the Ministry of Tourism Website
+          </h3>
+          <a
+            href="javascript:void(0)"
+            class="
+              text-body-color text-sm
+              font-semibold
+              py-3
+              px-7
+              inline-block
+              border
+              rounded-md
+              hover:bg-primary hover:border-primary hover:text-white
+              transition
+            "
+          >
+            View Details
+          </a>
+        </div>
+      </div>
+    </div>
+    <div
+      :class="showCards == 'all' || showCards == 'Laravel' || 'PHP' ? 'block' : 'hidden' "
+      class="w-full md:w-1/2 xl:w-1/3 px-4"
+    >
+      <div class="relative mb-12">
+        <div class="rounded-lg overflow-hidden ease-in duration-100 hover:scale-105">
+          <img
+            src="{{ url('/images/bot.jpeg') }}"
+            alt="covidbot"
+            class="w-full h-[260px] object-cover"
+          />
+        </div>
+        <div
+          class="
+          text-center bg-white dark:bg-slate-800 relative z-10 py-9 px-3 rounded-lg shadow-lg mx-7 -mt-4 h-[260px]
+          "
+        >
+          <span class="text-sm text-primary font-semibold block mb-2">
+            Laravel, PHP
+          </span>
+          <h3 class="font-bold text-xl text-dark mb-4 dark:text-white">
+            A Laravel Microservice that was build to showcase covidcase statuses around the world.
+          </h3>
+          <a
+            href="https://youtu.be/38fJX4oj9tk"
+            class="
+              text-body-color text-sm
+              font-semibold
+              py-3
+              px-7
+              inline-block
+              border
+              rounded-md
+              hover:bg-primary hover:border-primary hover:text-white
+              transition
+            "
+          >
+            View Details
+          </a>
+        </div>
+      </div>
+    </div>
+    <div
+      :class="showCards == 'all' || showCards == 'Oracle Apex' || 'Flutter' || 'iOS' || 'Android'  ? 'block' : 'hidden' "
+      class="w-full md:w-1/2 xl:w-1/3 px-4"
+    >
+    <div class="relative mb-12">
+      <div class="rounded-lg overflow-hidden ease-in duration-100 hover:scale-105">
+        <img
+          src="{{ url('/images/sac.png') }}"
+          alt="portfolio"
+          class="w-full h-[260px] object-cover"
+        />
+      </div>
+      <div
+        class="
+        text-center bg-white dark:bg-slate-800 relative z-10 py-9 px-3 rounded-lg shadow-lg mx-7 -mt-4 h-[260px]
+        "
+      >
+        <span class="text-sm text-primary font-semibold block mb-2">
+          Flutter, Oracle Apex, iOS, Android
+        </span>
+        <h3 class="font-bold text-xl text-dark mb-4 dark:text-white">
+          Mobile App for SacPackaging Customers to view packages, invoices, quotes easier.
+        </h3>
+        <a
+          href="javascript:void(0)"
+          class="
+            text-body-color text-sm
+            font-semibold
+            py-3
+            px-7
+            inline-block
+            border
+            rounded-md
+            hover:bg-primary hover:border-primary hover:text-white
+            transition
+          "
+        >
+          View Details
+        </a>
+      </div>
+    </div>
     </div>
   </div>
+</div>
 </section>
-<!-- ====== Project Section End -->
+<!-- ====== Portfolio Section End -->
